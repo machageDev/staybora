202490d8574709732ceae11a818f5f28330431e8
machageDev <machagefranklyn@gmail.com>
2025-06-07 21:32:01 -1200

    model pament

--- backend/core/models.py
+++ backend/core/models.py
@@ -47,3 +47,15 @@ class Location(models.Model):
     region = models.CharField(max_length=100)
     def __str__(self):
         return f"{self.name}, {self.region}, {self.county}"
+    
+class Payment(models.Model):
+    STATUS_CHOICES =[
+        ('pending', 'Pending'),
+        ('paid', 'Paid'),
+    ]
+    tenant = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default='pending')
+    def __str__(self):
+        return f"{self.tenant.username} - {self.amount}"
