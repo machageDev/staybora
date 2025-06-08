6b3426c3cd7155e34a6a01afc54458144171b07d
machageDev <machagefranklyn@gmail.com>
2025-06-07 21:54:57 -1200

    email backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -121,3 +121,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'          # or your email provider SMTP server
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your_email@gmail.com'   # your email
+EMAIL_HOST_PASSWORD = 'your_email_password'  # app password if 2FA enabled
