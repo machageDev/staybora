fe4c802f1bba3bd3e6aadc68d6c60f305ba86629
machageDev <machagefranklyn@gmail.com>
2025-06-07 21:11:11 -1200

    user models

--- backend/core/models.py
+++ backend/core/models.py
@@ -3,10 +3,12 @@ from django.db import models
 # Create your models here.
 from django.contrib.auth.models import User
 
-class User(models.Model):   
-    name = models.CharField(max_length=100)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)    
     email = models.EmailField(max_length=100)
     password = models.CharField(max_length=100)
-    
+    extra_info = models.CharField(max_length=100, blank=True, null=True)
+
     def __str__(self):
-        return self.password
+        return self.user.username
